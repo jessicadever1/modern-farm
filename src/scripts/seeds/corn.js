d6e3011b6e018a74860fb37f3a76c09b979cb702
jessicadever1 <jessicadever1@gmail.com>
2020-11-20 13:01:42 -0600

    added seed files, and worked on functions within them

---
+++ src/scripts/seeds/corn.js
@@ -0,0 +1,22 @@
+export const createCorn = () => {
+
+    const corn = [
+
+    /* The instructions say to return a value of two arrays, the arrays consist of two objects. Do I need to name these objects?*/
+
+        {
+            type: "corn",
+            height: 180,
+            output: 6
+        },
+
+        {
+            type: "corn",
+            height: 180,
+            output: 6
+        }
+    ]
+
+    return corn
+
+}
---
+++ src/scripts/tractor.js
@@ -0,0 +1,15 @@
+
+
+export const plantSeeds = (yearlyPlan) => {
+    let plants = [];
+    for (let i = 0; i < yearlyPlan.length; i++) {
+            for(j = 0; j < yearlyPlan[i].length; j++) {
+            
+            }
+    }
+    
+    return plants
+
+}
+
+const returnValueOfPlantSeeds = plantSeeds(activatePlan)
